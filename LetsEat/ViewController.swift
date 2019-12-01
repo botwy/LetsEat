dc52145dd384ba2f7a6f4839e7e97cc7988c66df
GONCHAROV-DVA <goncharov.d.vas@sberbank.ru>
2019-12-01 10:32:48 +0300

    Initial Commit

---
+++ LetsEat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LetsEat
+//
+//  Created by Гончаров Денис Васильевич on 01.12.2019.
+//  Copyright © 2019 Гончаров Денис Васильевич. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
